fd688e1b98316d61ed0dc9237b36bb3a3ec187cf
Raquel1241 <raquelsoniasimon@yahoo.com>
2026-02-24 12:56:26 -0500

    graded assesment added submissions

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
-    path('<int:course_id>/submit/', views.submit, name="submit")
+    path('<int:course_id>/submit/', views.submit, name="submit"),
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
